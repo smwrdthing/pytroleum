413f9c201af8abb4811382c291c9be0d2d8060e5
smwrdthing <amiraslanov16@gmail.com>
2025-11-20 15:11:05 +0300

    General factory + rearrangments

--- src/pytroleum/tdyna/eosi.py
+++ src/pytroleum/tdyna/eosi.py
@@ -9,10 +9,9 @@ else:
 import CoolProp
 import CoolProp.constants as CPconst
 
-
 # Realised we don't need subclass for hydrocarbon mixture interface definition at all.
-# in this case we're good to go with factory function. Though code for AbstractState subclassing
-# is retained in case of future needs
+# in this case we're good to go with factory function. Though code for AbstractState
+# subclassing is retained in case of future needs
 #
 # How to subclass AbstractState appropriately:
 # class CertainState(AbstractState):
@@ -29,8 +28,9 @@ import CoolProp.constants as CPconst
 #         # call signature here, should repeat one in __new__ to silence pylance
 #         pass
 
-# Maybe a default generic composition?
-GENERIC_HYDROCARBON_MIXTURE = [
+# Constants
+MOLE_FRACTION_SUM_TOL = 1e-3
+GENERIC_HYDROCARBS = {
     # Hydrocarbons
     'Methane',
     'Ethane',
@@ -45,18 +45,51 @@ GENERIC_HYDROCARBON_MIXTURE = [
     'CarbonMonoxide',
     'CarbonDioxide',
     'HydrogenSulfide'
-]
+}
 
 
-def hydrocarb_factory(composition: dict[str, float], backend: str = 'HEOS') -> AbstractState:
+def factory_eos(composition: dict[str, float], backend: str = 'HEOS',
+                with_state: None | Iterable = None) -> AbstractState:
 
-    # No assertions are imposed here
+    # should work both for mixtures and pures
+    names = '&'.join(list(composition.keys()))
 
-    hcm_name = '&'.join(list(composition.keys()))
-    hcm_frac = list(composition.values())
+    # Initially I did not want to specify mole fractions immediately, but decided to do so
+    mole_fractions = list(composition.values())
 
-    hcmix = AbstractState(backend, hcm_name)
-    hcmix.set_mole_fractions(hcm_frac)
+    # CoolProp does not enforce sum(mole_fractions) == 1, so we must do it
+    if np.abs(np.sum(mole_fractions) - 1) > MOLE_FRACTION_SUM_TOL:
+        msg = "Sum of mole fractions is not unity, provide valid composition"
+        raise ValueError(msg)
+
+    # If composition check is successfull, create interface and set mole fractions
+    eos = AbstractState(backend, names)
+    eos.set_mole_fractions(mole_fractions)
+
+    # If some state is given jump to it
+    if with_state is not None:
+        # Iterable of three is expected as with_state, first element - key of input pair,
+        # others — corresponding values. CoolProp should handle invalid with_state.
+        eos.update(*with_state)
+
+    return eos
+
+
+def factory_hydrocarbs(composition: dict[str, float], backend: str = 'PR',
+                       with_state: None | Iterable = None) -> AbstractState:
+
+    # Note how for general factory default CoolProp backend is Helmholtz EOS and for
+    # hydrocarbons it is Peng-Robinson EOS.
+
+    # Here we want to obtain an interface of hydrocarbon gases. Some assertions with
+    # regard to provided compostion should be done.
+    names = list(composition.keys())
+    if len(GENERIC_HYDROCARBS.intersection(names)) == 0:
+        msg = ("No components associated with typical natural gas")
+        raise ValueError(msg)
+
+    # If names are valid we create an interface with call to a common eos factory
+    hcmix = factory_eos(composition, backend, with_state)
 
     return hcmix
 
@@ -70,13 +103,13 @@ if __name__ == "__main__":
 
     natural_gas_composition = {
         'Methane': 0.6,
-        'Ethane': 0.2,
+        'Ethane': 0.1,
         'Propane': 0.1,
 
-        'Nitrogen': 0.05,
+        'Nitrogen': 0.1,
         'CO2': 0.05,
-        # 'CO': 0.04,
-        # 'H2S' : 0.01
+        'CO': 0.04,
+        'H2S': 0.01
     }
 
     # Check composition sanity
@@ -86,9 +119,10 @@ if __name__ == "__main__":
         msg = "Sum of mole fractions is more than 1"
         raise ValueError(msg) from e
 
-    # NOTE on molar fractions : CoolProp does absolutely nothing to ensure sum(mole_fractins) = 1
+    # NOTE on molar fractions : CoolProp does absolutely nothing to ensure
+    # sum(mole_fractins) = 1
 
-    hcm_eos = hydrocarb_factory(natural_gas_composition)
+    hcm_eos = factory_hydrocarbs(natural_gas_composition)
 
     print('Hydrocarbon composition')
     print(80*'=')
@@ -113,12 +147,12 @@ if __name__ == "__main__":
     ax.grid(True)
 
     # What if we change backend?
-    hcm_eos_PR = hydrocarb_factory(natural_gas_composition, 'PR')
+    hcm_eos_PR = factory_hydrocarbs(natural_gas_composition, 'PR')
     hcm_eos_PR.build_phase_envelope("")
     PE = hcm_eos_PR.get_phase_envelope_data()
     ax.plot(np.array(PE.T)-273.15, np.array(PE.p)/1e5)
 
-    hcm_eos_SRK = hydrocarb_factory(natural_gas_composition, 'SRK')
+    hcm_eos_SRK = factory_hydrocarbs(natural_gas_composition, 'SRK')
     hcm_eos_SRK.build_phase_envelope("")
     PE = hcm_eos_SRK.get_phase_envelope_data()
     ax.plot(np.array(PE.T)-273.15, np.array(PE.p)/1e5)
