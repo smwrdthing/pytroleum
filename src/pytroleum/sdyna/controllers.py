208f17972c8b89aa43b5d27bc9804b662b63745a
smwrdthing <amiraslanov16@gmail.com>
2025-11-21 15:09:21 +0300

    Implementation for PID controller

--- src/pytroleum/sdyna/controllers.py
+++ src/pytroleum/sdyna/controllers.py
@@ -1,15 +1,91 @@
 import abc
 import numpy as np
+from typing import Iterable
 
 
 class PropIntDiff:
 
-    def __init__(self, P, I, D, F, setpoint, saturation, ratelim) -> None:
+    def __init__(
+            self, P: float, I: float, D: float, F: float,  # noqa
+            setpoint: float, saturation: Iterable[float] = (-np.inf, np.inf),
+            ratelim: float = np.inf) -> None:
+
+        # noqa is needed because flake8 does not like I as variable name :
+        # ambiguous variable name 'I' (E741)
+        # but application is clear, so we retain this
+
+        # Assigning paramterers
         self.P, self.I, self.D = P, I, D
         self.F = F
         self.setpoint = setpoint
         self.saturation = saturation
         self.ratelim = ratelim
+
+        # Entries for active operation attributes
+        self.signal: float = 0
+        self.history_signal: float = 0
+
+        self.gain: float = 0
+        self.history_gain: float = 0
+
+        self.integral: float = 0
+        self.history_integral: float = 0
+
+        self.diff: float = 0
+        self.history_diff: float = 0
+
+        self.error: float = 0
+        self.history_error: float = 0
+
+        self.saturated: bool = False
+
+    def check_saturation(self):
+
+        upperlim, lowerlim = max(self.saturation), min(self.saturation)
+
+        self.saturated = False
+        if self.signal > upperlim:
+            self.saturated = True
+            self.signal = upperlim
+        elif self.signal < lowerlim:
+            self.saturated = True
+            self.signal = lowerlim
+
+    def check_rate(self, dt: float):
+
+        rate = (self.signal-self.history_signal)/dt
+        if abs(rate) > self.ratelim:
+            self.signal = self.history_signal+np.sign(rate)*self.ratelim*dt
+
+    def control(self, dt: float, probe: float, polarity: float = 1,
+                norm_by: float | None = None):
+
+        # Previous step values become history
+        self.history_gain = self.gain
+        self.history_integral = self.integral
+        self.history_diff = self.diff
+        self.history_error = self.error
+        self.history_signal = self.signal
+
+        # Computing new error
+        self.error = polarity*(self.setpoint-probe)
+
+        # If normalization factor is given - normalize error by it
+        if norm_by is not None:
+            self.error = self.error/norm_by
+
+        # Computing new signal
+        self.gain = self.P*self.error
+        self.integral = self.integral + self.I*self.error*dt
+        self.diff = self.D*(
+            self.error-self.history_error + self.F*self.history_diff)/(dt+self.F)
+        self.signal = self.gain+self.integral+self.error
+
+        # Performing saturation and rate limitataions checks and amends
+        self.check_saturation()
+        self.check_rate(dt)
+
+        # At this point signal for the next step is generated and can be employed
 
 
 class StartStop:
