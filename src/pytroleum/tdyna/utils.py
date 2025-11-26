354993e4ba0b6a447dfe672b8b4fcd38ff10d8ba
smwrdthing <amiraslanov16@gmail.com>
2025-11-26 10:41:41 +0300

    finished stub generator

--- src/pytroleum/tdyna/utils.py
+++ src/pytroleum/tdyna/utils.py
@@ -3,7 +3,7 @@ from typing import Any
 from CoolProp.CoolProp import AbstractState
 
 
-def generator_CoolStub(inspect: bool = True, force: bool = False) -> None:
+def generator_CoolStub(force: bool = False) -> None:
     """CoolStub.pyi generator
 
     Function to generate dummy CoolStub file. Instance of AbstractState class from
@@ -14,47 +14,46 @@ def generator_CoolStub(inspect: bool = True, force: bool = False) -> None:
     DISCLAIMER :
     This generator basically inspects dirs of objects and creates text file with .pyi
     extension containing class descriptions for classes of interests. Since it is
-    quite hard to make correct stub this function aims to generate "dummy" stub with most
-    types being defaulted to "Any" and most methods signatures defaulted to
+    quite hard to make correct stub this function aims to generate "dummy" stub with all
+    types being defaulted to "Any" and all methods signatures defaulted to
     (self, *args, **kwargs). User should adjust type annotations manually in generated
     file to represent expected inputs correctly.
 
     'CoolStub' name is hardcoded instead of accepting file name as input variable to
-    prevent user from creation of file with erroneous way. If different name requiered
-    consider renaming generated file name or changing hardcoded file name.
-
-    Destination directory is forced to be directory from which function was invoked for
-    same reasons.
+    prevent user from creation of file with erroneous name. If different name requiered
+    consider renaming generated file or changing hardcoded name in the function.
+    Destination directory is forced to be directory of the file containing this function
+    for same reason.
 
     Parameters
     ----------
-    inspect: bool = True
-        Variable to determine if generated strings should be printed in console before
-        file creation. By default writing is not allowed if inspection is not invoked.
-
     force: bool = False
         Variable to determine behavior of function. By default function checks if stub
         file with hardcoded name already exists. If it does - execution is aborted,
         otherwise new stub is generated and saved. If force is True check does not happen,
-        so if stub is already here it gets overriden.
+        so if stub is already here it will be overriden.
     """
-
-    # Check if file exist in case force option is False, abandon function if it does
-    if not force:
-        if 'CoolStub.pyi' in os.listdir():
-            print("CoolStub generator is abandoned, no new files were written")
-            return
 
     # Harcoded default name for stub
     fname = 'CoolStub.pyi'
+    # Get path where script resides
+    script_path = os.path.dirname(os.path.realpath(__file__))
+    # Construct path for file
+    fpath = script_path + '\\' + fname
+
+    # Check if file exist in case force option is False, abandon function if it does
+    if not force:
+        if 'CoolStub.pyi' in os.listdir(script_path):
+            print("CoolStub generator is abandoned, no new files were written")
+            return
 
     # We create an entry to objects, so we can access their dir. Passed arguments do not
     # really matter as we are interested in metadata of objects
     AS = AbstractState('HEOS', 'Methane&Ethane')
     AS.set_mole_fractions([0.6, 0.4])
 
-    # We want to create class in the stub for phase envelope too, as we plan to use it
-    # later
+    # We want to create class in the stub for phase envelope too, as we want to make
+    # a stub class for it as well
     AS.build_phase_envelope("")
     PE = AS.get_phase_envelope_data()
 
@@ -63,7 +62,8 @@ def generator_CoolStub(inspect: bool = True, force: bool = False) -> None:
     contentPE = dir(PE)
 
     # Now we create entries for stub-files texts, both start in the same fashion
-    importing_code = 'from typing import Any'  # for annotatinos
+    importing_code = 'from __future__ import annotations\n' + \
+        'from typing import Any\n'  # for annotatinos
     stubcodeAS = 'class AbstractState:\n'
     stubcodePE = 'class PyPhaseEnvelopeData:'
 
@@ -76,7 +76,7 @@ def generator_CoolStub(inspect: bool = True, force: bool = False) -> None:
     # Genration of stub file content for AbstractState class
     # Do __init__ outside of loop
     new_stub_method_code = (
-        '\n' + indent + space + 'def' + space + '__init__' +
+        '\n' + indent + 'def' + space + '__init__' +
         '(self, backend: str, fluid: str) -> None:' +
         '\n' + 2*indent + ellipsis + '\n'
     )
@@ -84,7 +84,7 @@ def generator_CoolStub(inspect: bool = True, force: bool = False) -> None:
     for method_name in contentAS:
         if '__' not in method_name:  # throw other dunders out of the window
             new_stub_method_code = (
-                '\n' + indent + space + 'def' + space + method_name +
+                '\n' + indent + 'def' + space + method_name +
                 default_args + '\n' + 2*indent + ellipsis + '\n'
             )
             stubcodeAS += new_stub_method_code
@@ -98,29 +98,9 @@ def generator_CoolStub(inspect: bool = True, force: bool = False) -> None:
             stubcodePE += new_attr_name
     stubcodePE += '\n'
 
-    stubcode = importing_code + '\n\n' + stubcodeAS + stubcodePE
+    stubcode = importing_code + '\n\n' + stubcodeAS + '\n' + stubcodePE
 
-    # No writing without inpection should be allowed, so writing_flag defaults to False
-    writing_flag = False
-    if inspect:
-        print(stubcode)
-        ans = " "
-        while ans not in 'yn':
-            ans = input("Is stub file content appropriate? [y/n] : ")
-        if ans == 'y':
-            writing_flag = True
-
-    # If writing_flag remains false abandon function
-    if not writing_flag:
-        print("CoolStub generator is abandoned, no new files were written")
-        return
-
-    # Otherwise write stub with message
+    # Writing stub
     print("Writing new stub for CoolProp...")
-    with open(fname, 'w', encoding='utf-8') as f:
+    with open(fpath, 'w', encoding='utf-8') as f:
         f.writelines(stubcode)
-
-
-if __name__ == '__main__':
-    # Trying to make stub
-    generator_CoolStub(force=True)
