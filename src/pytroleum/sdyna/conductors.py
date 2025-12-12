54c1af5cba700313c4a9a7fff9405d7245a26426
smwrdthing <amiraslanov16@gmail.com>
2025-12-12 12:15:40 +0300

    Valve flow resolution finishing touches

--- src/pytroleum/sdyna/conductors.py
+++ src/pytroleum/sdyna/conductors.py
@@ -134,12 +134,7 @@ class Valve(Conductor):
         self._diameter_valve = np.sqrt(4*new_area_vale/np.pi)
     # ------------------------------------------------------------------------------------
 
-    def compute_mass_flow_rate(self):
-
-        # It was implemented somewhat like this in legacy, but here we should find better
-        # way to handle multiphase situation and facilitate ODE system solution. Code is
-        # brought here in this form for preliminary considerations and should be
-        # reimplemented with smart utilization of phase ransparency idea
+    def compute_flow(self):
 
         upstream_state = self.source.state
         downstream_state = self.sink.state
@@ -165,6 +160,9 @@ class Valve(Conductor):
             )
         else:  # conductor deals with vapor phase
 
+            upstream_pressure = upstream_state.pressure[self.phase_index]
+            downstream_pressure = downstream_state.pressure[self.phase_index]
+
             mass_flow_rate = efflux.compressible(
                 self.area_valve*self.opening,
                 self.discharge_coefficient,
@@ -172,38 +170,23 @@ class Valve(Conductor):
                 # eos makes storing k and R excessive
                 upstream_state.equation_of_state[self.phase_index].cvmass() /
                 upstream_state.equation_of_state[self.phase_index].cpmass(),
-                upstream_state.equation_of_state[self.phase_index].gas_constant(
-                ),
+                R/upstream_state.equation_of_state[self.phase_index].molar_mass(),
 
                 upstream_state.density[self.phase_index],
                 upstream_state.temperature[self.phase_index],
-                upstream_state.pressure[self.phase_index],
+                upstream_pressure,
+
                 downstream_state.density[self.phase_index],
                 downstream_state.temperature[self.phase_index],
-                downstream_state.pressure[self.phase_index]
+                downstream_pressure
             )
 
-        self.flow.mass_flow_rate[self.phase_index] = mass_flow_rate
-
-    def compute_energy_flux(self):
-        # j = (u+p/rho+w^2/2)*G
-        if self.flow.mass_flow_rate >= 0:
+        if mass_flow_rate >= 0:
             donor = self.source.state
         else:
             donor = self.sink.state
 
-        # NOTE this violates DRY, find better way to do this later
-        valve_opening = 1
-        if self.controller is not None:
-            valve_opening = self.controller.signal
-
-        # NOTE I am inclined to make FlowData fields scalar now hmm, should thin this
-        # through better, pretty large change. Try to write as if single value kept for
-        # FlowData and see what happens
-        #
-        # On the other hand we have a pretty solid reason to keep array interfaces : inlet
-        # flow rates are multiphase. This thing alone probably stumps all the reasoning
-        # I suppose... Add to it opportunity to account for flow
+        self.flow.mass_flow_rate[self.phase_index] = mass_flow_rate
 
         (
             self.flow.energy_specific[self.phase_index],
@@ -214,22 +197,21 @@ class Valve(Conductor):
             donor.temperature[self.phase_index],
             donor.density[self.phase_index]
         )
-        self.flow.pressure[self.phase_index] = np.interp(
-            self.elevation,
-            [0, *np.flip(donor.level)],
-            [*np.flip(donor.pressure), donor.pressure[0]]
-        )
-        self.flow.velocity[self.phase_index] = (self.flow.mass_flow_rate /
-                                                self.flow.density /
-                                                (self.area_valve*valve_opening))
-        self.flow.energy_specific_flow = (self.flow.energy_specific +
-                                          self.flow.pressure/self.flow.density +
-                                          self.flow.velocity**2/2)
+
+        self.flow.pressure[self.phase_index] = max(
+            upstream_pressure, downstream_pressure)
+        self.flow.velocity[self.phase_index] = (
+            self.flow.mass_flow_rate[self.phase_index] /
+            self.flow.density[self.phase_index] /
+            self.area_pipe)
+        self.flow.energy_specific_flow[self.phase_index] = (
+            self.flow.energy_specific[self.phase_index] +
+            self.flow.pressure[self.phase_index] / self.flow.density[self.phase_index] +
+            self.flow.velocity[self.phase_index]**2/2)
         self.flow.energy_flow = self.flow.energy_specific_flow*self.flow.mass_flow_rate
 
     def advance(self):
-        self.compute_mass_flow_rate()
-        self.compute_energy_flux()
+        self.compute_flow()
 
 
 class CentrifugalPump(Conductor):
