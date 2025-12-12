3e7a4d87fac3c4373fe2113b7a80a2a23247dc85
smwrdthing <amiraslanov16@gmail.com>
2025-12-12 11:52:25 +0300

    initializer overloads for Conductor

--- src/pytroleum/sdyna/conductors.py
+++ src/pytroleum/sdyna/conductors.py
@@ -18,12 +18,27 @@ from numpy import float64
 class Conductor(ABC):
 
     # Abstract base class for conductor
-
-    @abstractmethod
+    @overload
     def __init__(self, phase_index: int,
                  source: ControlVolume | None = None,
                  sink: ControlVolume | None = None) -> None:
         if source is None:
+        ...
+
+    @overload
+    def __init__(self, phase_index: list[int],
+                 source: ControlVolume | None = None,
+                 sink: ControlVolume | None = None) -> None:
+        ...
+
+    @abstractmethod
+    def __init__(self, phase_index,
+                 source=None,
+                 sink=None) -> None:
+
+        # Possible TODO
+        # default flow attribute in FlowData
+
             from pytroleum.sdyna.convolumes import Atmosphere
             self.source = Atmosphere()
         if sink is None:
