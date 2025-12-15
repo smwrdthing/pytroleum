261814dd5a97d235ecc02cb60d2d33057ff6cbb2
my_nicknameValeria <leralarchen08@gmail.com>
2025-12-15 11:30:12 +0300

    Learning commititng changes

--- tests/dummy_test.py
+++ tests/dummy_test.py
@@ -1,1 +1,15 @@
 import pytroleum.meter as meter
+
+# This file is for learning only
+
+class TestThisFunction:
+
+    def __init__(self):
+        pass
+
+# Another class
+
+class TestAnotherFunction:
+
+    def __init__(self):
+        pass
