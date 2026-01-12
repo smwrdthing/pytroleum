trying to fix overflow computations

src/pytroleum/sdyna/conductors.py
@@ -650,8 +650,8 @@ class OverPass(Conductor):
         self.flow.mass_flow_rate[of_phase] = 0
         if self.is_reached:
             # collect net flow rates for source formed by other conductors
-            other_liquid_flow_rates_inlet = 0
-            other_liquid_flow_rates_outlet = 0
+            other_liquid_flow_rates_inlet = np.array(0)
+            other_liquid_flow_rates_outlet = np.array(0)
             for inlet in self.source.inlets:
                 if inlet is not self:
                     other_liquid_flow_rates_inlet += inlet.flow.mass_flow_rate[1:]
@@ -659,9 +659,12 @@ class OverPass(Conductor):
                 if outlet is not self:
                     other_liquid_flow_rates_outlet += outlet.flow.mass_flow_rate[1:]
 
-            overflow_rate = self.source.state.density[1]*np.sum(
-                (other_liquid_flow_rates_inlet-other_liquid_flow_rates_outlet) /
-                self.source.state.density)
+            other_liquid_net_flow = (
+                other_liquid_flow_rates_inlet - other_liquid_flow_rates_outlet)
+
+            overflow_rate = other_liquid_net_flow[of_phase] - np.sum(
+                self.source.state.density[of_phase] / self.source.state.density *
+                other_liquid_net_flow)
 
             if overflow_rate < 0:
                 # NOTE : assigining integer 0 potentially can lead to troubles if numpy