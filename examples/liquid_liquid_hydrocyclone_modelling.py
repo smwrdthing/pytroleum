0b8b46346a863f3490c0fb540e657abdbc7cfa56
smwrdthing <amiraslanov16@gmail.com>
2026-03-03 11:20:15 +0300

    example uses fabric now

--- examples/liquid_liquid_hydrocyclone_modelling.py
+++ examples/liquid_liquid_hydrocyclone_modelling.py
@@ -12,7 +12,7 @@ def compute_resistance(density, area, discharge_coeff):
 
 
 # Instantiate design, flowsheet and velocity field
-design = llh.SouthamptonDesign(80e-3)  # 80 mm Colman-Thew hydrocyclone
+design = llh.southampton_design_factory(80e-3)
 flowsheet = llh.FlowSheet()
 velocity_field = llh.VelocityField()
 
