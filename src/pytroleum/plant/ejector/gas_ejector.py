8739b74ab6127e38c611105dde5275800346c94e
my_nicknameValeria <leralarchen08@gmail.com>
2026-05-05 16:14:23 +0300

    adding calculation of common parameters

--- src/pytroleum/plant/ejector/gas_ejector.py
+++ src/pytroleum/plant/ejector/gas_ejector.py
@@ -39,6 +39,14 @@ class Ejector:
     entrainment_ratio: float
     critical_pressure: float
     critical_temperature: float
+    m1: float
+    m: float
+    n: float
+    dynamic_head_nozzle_exit: float
+    ejector_head_no_diff: float
+    pressure_cyl_section_exit: float
+    temperature_cyl_section_exit: float
+    nozzle_exit_velocity: float
 
 
 def operation_conditions(active: ActiveMediumData, passive: PassiveMediumData,
@@ -48,6 +56,15 @@ def operation_conditions(active: ActiveMediumData, passive: PassiveMediumData,
 
     # Коэфициент эжекции
     entrainment_ratio = passive.mass_flow/active.mass_flow
+
+    # Основное уравнение эжекции для участка струи от сопла до места
+    # соприкосновения со стенкой
+    m1 = 2*(1+entrainment_ratio)**2
+
+    # Основной геометрический параметр эжектора m
+    m = m1/(1+(2*entrainment_ratio**2)/m1)
+
+    n = m/m1
 
     # Газовая постоянная R для активной и пассивной среды
     R_active = calculate_gas_constant(active.molecular_mass)
@@ -74,7 +91,31 @@ def operation_conditions(active: ActiveMediumData, passive: PassiveMediumData,
     critical_temperature = active.temperature * \
         critical_pressure_ratio**((adiabatic_index-1)/adiabatic_index)
 
+    # Динамический напор эжектирующей струи на выходе из сопла (сечение I-I):
+    dynamic_head_nozzle_exit = active.inlet_pressure/1.1
+
+    # Напор создаваемый эжектором без диффузора
+    ejector_head_no_diff = dynamic_head_nozzle_exit/m
+
+    # Давление в конце цилиндрического участка (сечение III-III):
+    pressure_cyl_section_exit = ejector_head_no_diff-passive.inlet_pressure
+
+    # Температура в конце цилиндрического участка
+    temperature_cyl_section_exit = critical_temperature * \
+        (pressure_cyl_section_exit/critical_pressure)**(adiabatic_index-1)/adiabatic_index
+
+    # Скорость истечения газа из сопла:
+    nozzle_exit_velocity = np.sqrt(
+        2*g*dynamic_head_nozzle_exit/calculate_specific_weight(active.density))
+
     return Ejector(compression_ratio=compression_ratio,
                    entrainment_ratio=entrainment_ratio,
                    critical_pressure=critical_pressure,
-                   critical_temperature=critical_temperature)
+                   critical_temperature=critical_temperature,
+                   m1=m1,
+                   m=m, n=n,
+                   dynamic_head_nozzle_exit=dynamic_head_nozzle_exit,
+                   ejector_head_no_diff=ejector_head_no_diff,
+                   pressure_cyl_section_exit=pressure_cyl_section_exit,
+                   temperature_cyl_section_exit=temperature_cyl_section_exit,
+                   nozzle_exit_velocity=nozzle_exit_velocity)
